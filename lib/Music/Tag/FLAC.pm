cbc8309089a07387d204e8320bd2a1ff0a400332
Edward Allen <ealleniii@cpan.org>
2010-07-25 19:56:44 -0700

    pod move to end of file

--- lib/Music/Tag/FLAC.pm
+++ lib/Music/Tag/FLAC.pm
@@ -9,6 +9,100 @@ our $VERSION = 0.32;
 #
 
 
+use strict;
+use Audio::FLAC::Header;
+
+#use Image::Magick;
+use base qw(Music::Tag::Generic);
+
+sub flac {
+	my $self = shift;
+	unless ((exists $self->{_Flac}) && (ref $self->{_Flac})) {
+		if ($self->info->filename) {
+			$self->{_Flac} = Audio::FLAC::Header->new($self->info->filename);
+		}
+	}
+	return $self->{_Flac};
+
+}
+
+our %tagmap = (
+	TITLE	=> 'title',
+	TRACKNUMBER => 'track',
+	TRACKTOTAL => 'totaltracks',
+	ARTIST => 'artist',
+	ALBUM => 'album',
+	COMMENT => 'comment',
+	DATE => 'releasedate',
+	GENRE => 'genre',
+	DISC => 'disc',
+	LABEL => 'label',
+	ASIN => 'asin',
+    MUSICBRAINZ_ARTISTID => 'mb_artistid',
+    MUSICBRAINZ_ALBUMID => 'mb_albumid',
+    MUSICBRAINZ_TRACKID => 'mb_trackid',
+    MUSICBRAINZ_SORTNAME => 'sortname',
+    RELEASECOUNTRY => 'countrycode',
+    MUSICIP_PUID => 'mip_puid',
+    MUSICBRAINZ_ALBUMARTIST => 'albumartist'
+);
+ 
+sub get_tag {
+    my $self     = shift;
+    if ( $self->flac ) {
+		while (my ($t, $v) = each %{$self->flac->tags}) {
+			if ((exists $tagmap{$t}) && (defined $v)) {
+				my $method = $tagmap{$t};
+				$self->info->$method($v);
+			}
+		}
+        $self->info->secs( $self->flac->{trackTotalLengthSeconds} );
+        $self->info->bitrate( int($self->flac->{bitRate} / 1000) );
+
+		#"MIME type"     => The MIME Type of the picture encoding
+		#"Picture Type"  => What the picture is off.  Usually set to 'Cover (front)'
+		#"Description"   => A short description of the picture
+		#"_Data"	       => The binary data for the picture.
+        if (( $self->flac->picture) && ( not $self->info->picture_exists)) {
+			my $pic = $self->flac->picture;
+            $self->info->picture( {
+					"MIME type" => $pic->{mimeType},
+					"Picture Type" => $pic->{description},
+					"_Data"	=> $pic->{imageData},
+				});
+        }
+    }
+    return $self;
+}
+
+sub set_tag {
+    my $self = shift;
+    if ( $self->flac ) {
+		while (my ($t, $v) = each %tagmap) {
+			if (defined $self->info->$v) {
+				$self->flac->tags->{$t} = $self->info->$v;
+			}
+		}
+        $self->flac->write();
+    }
+    return $self;
+}
+
+sub close {
+	my $self = shift;
+	delete $self->{_Flac};
+}
+
+sub default_options {
+   {
+   	wav_out_system => [ "flac", "-cd", "-c", "[FILENAME]" ],
+   }
+}
+
+1;
+
+# vim: tabstop=4
+__END__
 =pod
 
 =for changes stop
@@ -73,25 +167,6 @@ No values are required (except filename, which is usually provided on object cre
 
 =head1 SET DATA VALUES
 
-=cut
-
-use strict;
-use Audio::FLAC::Header;
-
-#use Image::Magick;
-use base qw(Music::Tag::Generic);
-
-sub flac {
-	my $self = shift;
-	unless ((exists $self->{_Flac}) && (ref $self->{_Flac})) {
-		if ($self->info->filename) {
-			$self->{_Flac} = Audio::FLAC::Header->new($self->info->filename);
-		}
-	}
-	return $self->{_Flac};
-
-}
-
 =over 4
 
 =item title, track, totaltracks, artist, album, comment, releasedate, genre, disc, label
@@ -110,82 +185,11 @@ Uses MusicBrainz recommended tags for these.
 
 Gathers this info from file.  Please note that secs is fractional.
 
-=cut
-
-our %tagmap = (
-	TITLE	=> 'title',
-	TRACKNUMBER => 'track',
-	TRACKTOTAL => 'totaltracks',
-	ARTIST => 'artist',
-	ALBUM => 'album',
-	COMMENT => 'comment',
-	DATE => 'releasedate',
-	GENRE => 'genre',
-	DISC => 'disc',
-	LABEL => 'label',
-	ASIN => 'asin',
-    MUSICBRAINZ_ARTISTID => 'mb_artistid',
-    MUSICBRAINZ_ALBUMID => 'mb_albumid',
-    MUSICBRAINZ_TRACKID => 'mb_trackid',
-    MUSICBRAINZ_SORTNAME => 'sortname',
-    RELEASECOUNTRY => 'countrycode',
-    MUSICIP_PUID => 'mip_puid',
-    MUSICBRAINZ_ALBUMARTIST => 'albumartist'
-);
- 
-sub get_tag {
-    my $self     = shift;
-    if ( $self->flac ) {
-		while (my ($t, $v) = each %{$self->flac->tags}) {
-			if ((exists $tagmap{$t}) && (defined $v)) {
-				my $method = $tagmap{$t};
-				$self->info->$method($v);
-			}
-		}
-        $self->info->secs( $self->flac->{trackTotalLengthSeconds} );
-        $self->info->bitrate( int($self->flac->{bitRate} / 1000) );
-
 =pod
 
 =item picture
 
 This is currently read-only.
-
-=cut
-
-		#"MIME type"     => The MIME Type of the picture encoding
-		#"Picture Type"  => What the picture is off.  Usually set to 'Cover (front)'
-		#"Description"   => A short description of the picture
-		#"_Data"	       => The binary data for the picture.
-        if (( $self->flac->picture) && ( not $self->info->picture_exists)) {
-			my $pic = $self->flac->picture;
-            $self->info->picture( {
-					"MIME type" => $pic->{mimeType},
-					"Picture Type" => $pic->{description},
-					"_Data"	=> $pic->{imageData},
-				});
-        }
-    }
-    return $self;
-}
-
-sub set_tag {
-    my $self = shift;
-    if ( $self->flac ) {
-		while (my ($t, $v) = each %tagmap) {
-			if (defined $self->info->$v) {
-				$self->flac->tags->{$t} = $self->info->$v;
-			}
-		}
-        $self->flac->write();
-    }
-    return $self;
-}
-
-sub close {
-	my $self = shift;
-	delete $self->{_Flac};
-}
 
 =back
 
@@ -200,14 +204,6 @@ None currently.
 =item default_options
 
 Returns the default options for the plugin.  
-
-=cut
-
-sub default_options {
-   {
-   	wav_out_system => [ "flac", "-cd", "-c", "[FILENAME]" ],
-   }
-}
 
 =item set_tag
 
@@ -236,73 +232,13 @@ Plugin does not fully support all fields I would like.  Pictures are read only (
 L<Audio::FLAC::Header>, L<Music::Tag>, L<Music::Tag::Amazon>, L<Music::Tag::File>, L<Music::Tag::Lyrics>,
 L<Music::Tag::M4A>, L<Music::Tag::MP3>, L<Music::Tag::MusicBrainz>, L<Music::Tag::OGG>, L<Music::Tag::Option>
 
-=head1 CHANGES
+=head1 SOURCE
 
-=for changes continue
+Source is available at github: L<http://github.com/riemann42/Music-Tag-FLAC|http://github.com/riemann42/Music-Tag-FLAC>.
 
-=over 4
+=head1 BUGTRACKING
 
-=item Release Name: 0.32
-
-=over 4
-
-=item *
-
-Changed license to allow option of GPL
-
-=item *
-
-Started using Pod::Readme
-
-=back
-
-=begin changes
-
-=item Release Name: 0.31
-
-=over 4
-
-=item * 
-
-Kwalitee Changes
-
-=back
-
-=item Release Name: 0.30
-
-=over 4
-
-=item * 
-
-Fixed packaging issues
-
-=back
-
-=item Release Name: 0.29
-
-=over 4
-
-=item * 
-
-Fixed name of module in POD
-
-=back
-
-=item Release Name: 0.28
-
-=over 4
-
-=item * 
-
-Split off from Music::Tag distribution
-
-=back
-
-=end changes
-
-=back
-
-=for readme continue
+Please use github for bug tracking: L<http://github.com/riemann42/Music-Tag-FLAC/issues|http://github.com/riemann42/Music-Tag-FLAC/issues>.
 
 =head1 AUTHOR 
 
@@ -338,8 +274,3 @@ http://www.gnu.org/copyleft/gpl.html.
 
 Copyright (c) 2007,2008 Edward Allen III. Some rights reserved.
 
-=cut
-
-1;
-
-# vim: tabstop=4
